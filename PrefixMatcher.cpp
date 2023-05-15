5d377157567336c8a19de50454c225b238e4f10a
han22232 <109938669+han22232@users.noreply.github.com>
2023-05-15 23:47:45 +0930

    Add files via upload

---
+++ PrefixMatcher.cpp
@@ -0,0 +1,51 @@
+
+#include "PrefixMatcher.h"
+
+PrefixMatcher::PrefixMatcher() {
+    root = new TrieNode();
+}
+
+PrefixMatcher::~PrefixMatcher() {
+    deleteTrie(root);
+}
+
+void PrefixMatcher::insert(std::string address, int routerNumber) {
+    TrieNode* curr = root;
+    for (char c : address) {
+        if (curr->children.count(c) == 0) {
+            curr->children[c] = new TrieNode();
+        }
+        curr = curr->children[c];
+    }
+    curr->routerNumber = routerNumber;
+}
+
+int PrefixMatcher::selectRouter(std::string networkAddress) {
+    TrieNode* curr = root;
+    int routerNumber = -1;
+    int i = 0;
+
+    while (curr != nullptr && i < networkAddress.size()) {
+        char c = networkAddress[i];
+        if (curr->children.count(c) == 0) {
+            break;
+        }
+        curr = curr->children[c];
+        if (curr->routerNumber != -1) {
+            routerNumber = curr->routerNumber;
+        }
+        i++;
+    }
+
+    return routerNumber;
+}
+
+
+void PrefixMatcher::deleteTrie(TrieNode* node) {
+    if (node == nullptr) {
+        return;
+    }
+    deleteTrie(node->children[0]);
+    deleteTrie(node->children[1]);
+    delete node;
+}
